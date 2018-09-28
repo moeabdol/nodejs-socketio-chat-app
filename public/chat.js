f4d318890f71c28937945d7e986d3c80ec6ca8b7
moeabdol <mohd.a.saed@gmail.com>
2018-09-28 23:06:40 +0300

    Broadcasting 'typing' event

--- public/chat.js
+++ public/chat.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
   var handle     = $('#handle');
   var sendButton = $('#send');
   var output     = $('#output');
+  var feedback   = $('#feedback');
 
   sendButton.click(function(e) {
     e.preventDefault();
@@ -12,10 +13,24 @@ $(document).ready(function() {
       handle: handle.val(),
       message: message.val()
     });
+
+    message.val('');
+  });
+
+  message.keypress(function() {
+    socket.emit('typing', {
+      handle: handle.val()
+    });
   });
 
   socket.on('chat', function(data) {
+    feedback.html('');
     output.append('<p><strong>' + data.handle + ': ' + '</strong>' +
       data.message + '</p>');
+  });
+
+  socket.on('typing', function(data) {
+    feedback.html('<p><em>' + data.handle +
+      ' is typing a message...</em></p>');
   });
 });
